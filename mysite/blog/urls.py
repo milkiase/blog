db35f92d00420cbade39eb676bd290491511c5f5
milkiase <milktes27@gmail.com>
2024-01-11 14:26:08 +0300

    added the detail and new post pages

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import post_list
+from .views import post_list, post_detail, post_new
 
 urlpatterns = [
-    path('', post_list, name='post_list')
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
 from .models import Post
+from .forms import PostForm
 from django.utils import timezone
 
 # Create your views here.
@@ -8,3 +9,21 @@ def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.filter().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_list_or_404(Post , pk=pk)[0]
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if(request.method == 'POST'):
+        form = PostForm(request.POST)
+        if(form.is_valid()):
+            post = form.save(commit=False)
+            post.published_date = timezone.now()
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_add_edit.html', {'form': form})
+
